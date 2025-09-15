51434c00806cd34cf4f0c50a55553eb32f7c2b7e
Wltb13 <weltoncado@gmail.com>
2025-09-15 14:18:23 -0300

    index.js

--- index.js
+++ index.js
@@ -138,10 +138,11 @@ app.post('/webhook', (req, res) => {
   `🍧 Pedido ${i + 1}\n` +
   `🥤 Tamanho: ${p.tamanho || '*não informado* ⚠️'}\n` +
   `🍫 Complementos:\n${
-  Array.isArray(p.complementos) && p.complementos.length > 0
-    ? p.complementos.map(c => `  - ${c}`).join('\n')
-    : '  - não informado ⚠️'
-}\n` +
+    Array.isArray(p.complementos) && p.complementos.length > 0
+      // Mostra só os nomes, sem número
+      ? p.complementos.map(c => `  - ${c}`).join('\n')
+      : '  - não informado ⚠️'
+  }\n` +
   `💰 Pagamento: ${p.pagamento || '*não informado* ⚠️'}\n` +
   `🏠 Endereço: ${p.endereco || '*não informado* ⚠️'}\n`
 )).join('\n');
